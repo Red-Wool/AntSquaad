6802ad13935f85fb27a6dc6f43aaad1bd689f444
RedWool <leolui0930.sia@gmail.com>
2025-10-28 19:30:23 -0400

    Camera now follow and pans out when multiple ants are connected

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -2,6 +2,20 @@ class_name CameraScript extends Camera2D
 
 @onready var player_follow : PlayerAnt = %PlayerAnt
 
+@export var follow_speed : float
+@export var zoom_speed : float
+@export var zoom_distance : float
 
-#func _process(delta):
+func _process(delta):
+	var follow : Vector2 = player_follow.position
+	var furthest_point_distance : float = 0
 	
+	for ant : Ant in player_follow.connected_ants:
+		follow += ant.position
+		
+		if (position - ant.position).length() > furthest_point_distance:
+			furthest_point_distance = (position - ant.position).length()
+	
+	follow /= 1 + player_follow.connected_ants.size()
+	position = lerp(position, follow, delta * follow_speed)
+	zoom = lerp(zoom, Vector2.ONE / max(1, furthest_point_distance / zoom_distance) , delta * zoom_speed)
