dccf331b29541618cc4e6bea5144a60f30ebf817
RedWool <leolui0930.sia@gmail.com>
2025-11-04 15:31:30 -0500

    Fixed camera not properly addjusting when ant dies

--- Scripts/Ants/Ant.gd
+++ Scripts/Ants/Ant.gd
@@ -5,6 +5,8 @@ var is_player_connected : bool = false
 var connected_ant : PlayerAnt
 
 func _connect_ant(player : PlayerAnt):
+	is_player_connected = true
+	connected_ant = player
 	player._connect_ant(self)
 
 func _connected_movement(movement : Vector2):
@@ -12,6 +14,7 @@ func _connected_movement(movement : Vector2):
 	move_and_slide()
 
 func _death():
+	print("death")
 	if is_player_connected:
 		connected_ant._connected_ant_death(self)
 	super()
